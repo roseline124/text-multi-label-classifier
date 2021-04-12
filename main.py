025225bb3f411997d6f6e4afaa6c6e26843aafe9
roseline124 <guseod24@gmail.com>
2021-04-12 19:23:03 +0900

    :sparkles: fast api: tag 추천하는 api 추가

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from classifier import text_multi_label_classifier
+import pandas as pd
 
 app = FastAPI()
+clf, feature_transformer, multilabel = text_multi_label_classifier.classifier()
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"msg": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/tags/{post_id}")
+async def read_post_tags(post_id: int, title: Optional[str] = None, tags: Optional[str] = None):
+    df_test = pd.DataFrame(
+        {'title': [title], 'external_tags': [tags]})
+    Xt = feature_transformer.transform(df_test)
+    result = multilabel.inverse_transform(clf.predict(Xt))
+    return {"tags": str(result)}
